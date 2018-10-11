0ca5debc7258609fc97f4306191316f20d0929c8
Kevin Mullen <kevin@kevins-macbook-pro-15.local>
2018-10-11 05:58:58 -0700

    Initial Commit

---
+++ BaseTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseTimer
+//
+//  Created by Kevin Mullen on 10/11/18.
+//  Copyright © 2018 Kevin Mullen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
